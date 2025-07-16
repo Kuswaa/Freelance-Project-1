82ea788aad724b57d2789b85c5efc1e7d541b255
Kuswaa <kuswatulkaaba@gmail.com>
2025-07-16 21:59:59 +0500

    home page UI complete and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
+module.exports =
+{
+  content: 
+  [
+    "./src/**/*.{html,ts}",
   ],
-  theme: {
+  
+  theme:
+  {
     extend: {},
+    screens:
+    {
+      'lg': '1024px',
+      'md': {'max': '1023px'},
+      'sm': {'max': '767px'},
+      'xs': {'max': '425px'},
+    },
   },
-  plugins: [],
+  
+  plugins: [
+    
+  ],
+  
+  darkMode: 'class'
 }
