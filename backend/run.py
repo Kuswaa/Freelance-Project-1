304f5e3a60f5ef293c1c612401f4d9bd5aeb69cc
Kuswaa <kuswatulkaaba@gmail.com>
2025-09-10 16:14:48 +0500

    deploying to heroku...

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    # Use platform PORT or fallback to 5000
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
